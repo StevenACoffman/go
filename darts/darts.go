1a5e3a7506414ef633952ba233593108cf05cb6d
Dave McAlpine <dave@Daves-MBP.gha.chartermi.net>
2019-11-06 16:52:56 -0500

    - Got darts test passing (and fast too!) - Stubbed out phone-number methods
    Signed-off-by: Dave McAlpine <dave@Daves-MBP.gha.chartermi.net>

--- darts/darts.go
+++ darts/darts.go
@@ -1,1 +1,25 @@
 package darts
+
+import (
+	"math"
+)
+
+/*
+The outer circle has a radius of 10 units
+the middle circle a radius of 5 units,
+and the inner circle a radius of 1
+*/
+func Score(x, y float64) (score int) {
+	switch distance := math.Sqrt((x * x) + (y * y)); {
+	case distance <= 1.0:
+		score = 10
+	case distance <= 5.0:
+		score = 5
+	case distance <= 10.0:
+		score = 1
+	default:
+		score = 0
+	}
+
+	return score
+}
--- phone-number/phone_number.go
+++ phone-number/phone_number.go
@@ -1,1 +1,13 @@
 package phonenumber
+
+func Number(num string) (string, error) {
+	return num, nil
+}
+
+func AreaCode(num string) (string, error) {
+	return "", nil
+}
+
+func Format(num string) (string, error) {
+	return "", nil
+}
