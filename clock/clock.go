e67ab5f04419708e5094269316bda2728c64142f
Jason Voll <jason.voll1441@gmail.com>
2019-11-06 13:42:05 -0800

    Compliing clock

--- clock/clock.go
+++ clock/clock.go
@@ -1,1 +1,27 @@
 package clock
+
+import "fmt"
+
+type Clock struct {
+	Hour   int
+	Minute int
+}
+
+func New(hour, minute int) Clock {
+	return Clock{
+		Hour:   hour,
+		Minute: minute,
+	}
+}
+
+func (c Clock) String() string {
+	return fmt.Sprintf("%v:%v", c.Hour, c.Minute)
+}
+
+func (c Clock) Add(minutes int) Clock {
+	return c
+}
+
+func (c Clock) Subtract(minutes int) Clock {
+	return c
+}
