0402a65739826a20fe8e22d7fc1c2fd4701893a1
Kathy Philip <katherinephilip@khanacademy.org>
2019-11-06 12:51:41 -0800

    darts tests work

--- darts/darts.go
+++ darts/darts.go
@@ -1,1 +1,22 @@
 package darts
+
+import "math"
+
+// Score returns the score
+func Score(x, y float64) int {
+	switch {
+	case inCircle(x, y, 1):
+		return 10
+	case inCircle(x, y, 5):
+		return 5
+	case inCircle(x, y, 10):
+		return 1
+	default:
+		return 0
+	}
+}
+
+func inCircle(x, y, radius float64) bool {
+	dist := math.Sqrt(x*x + y*y)
+	return dist <= radius
+}
