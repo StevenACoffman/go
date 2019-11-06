1741c8306db1a33ee895a51c2d3531add74089c5
Ragini Gupta <ragini@khanacademy.org>
2019-11-06 13:42:53 -0800

    Solution to phone-number

--- phone-number/phone_number.go
+++ phone-number/phone_number.go
@@ -1,1 +1,44 @@
 package phonenumber
+
+import (
+	"strings"
+	"errors"
+)
+
+func Number(input string) (string, error) {
+	output := strings.Replace(input, " ", "", -1)
+	output = strings.Replace(output, "(", "", -1)
+	output = strings.Replace(output, ")", "", -1)
+	output = strings.Replace(output, ".", "", -1)
+	output = strings.Replace(output, "-", "", -1)
+	output = strings.Replace(output, "+", "", -1)
+
+	if len(output) > 10 {
+		output = output[1:]
+	}
+	return output, nil
+}
+
+func Format(input string) (string, error) {
+	initial, err := Number(input)
+	if err != nil {
+		return "", err
+	}
+	formatted := "(" + initial[0:3] + ") " +initial[3:6] + "-" + initial[6:]
+	return formatted, nil
+}
+
+func AreaCode(input string) (string, error) {
+	number, errNumber := Number(input)
+
+	if errNumber != nil {
+		return "", errNumber
+	}
+
+	if len(number) != 10 {
+		return "", errors.New("error")
+	}
+
+	areaCode := number[0:3]
+	return areaCode, nil
+}
