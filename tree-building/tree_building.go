c5f5e165f05c1b134258fdf77e6df5e5d2add4fa
Aasmund Eldhuset <aasmund.eldhuset@gmail.com>
2019-11-06 13:42:20 -0800

    Tree, almost

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,1 +1,47 @@
 package tree
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Record struct {
+	ID int
+	Parent int
+}
+
+type Node struct {
+	ID int
+	Children []*Node
+}
+
+func Build(records []Record) (*Node, error) {
+	nodes := make(map[int]*Node)
+	var root *Node
+	for _, rec := range records {
+		node, ok := nodes[rec.ID]
+		if !ok {
+			node = &Node{ID: rec.ID}
+			nodes[rec.ID] = node
+		} else {
+			return nil, fmt.Errorf("dup")
+		}
+		if rec.Parent != rec.ID {
+			parent, ok := nodes[rec.Parent]
+			if !ok {
+				parent = &Node{ID: rec.Parent}
+				nodes[rec.Parent] = parent
+				parent.Children = append(parent.Children, node)
+				sort.Slice(parent.Children, func(i, j int) bool {
+					return parent.Children[i].ID < parent.Children[j].ID
+				})
+			}
+		} else {
+			root = node
+		}
+	}
+	if root == nil && len(records) != 0 {
+		return nil, fmt.Errorf("root")
+	}
+	return root, nil
+}
