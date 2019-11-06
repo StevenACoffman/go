e7ef5d7e50bb05674148030db8d85c4cf51b1300
Jason Voll <jason.voll1441@gmail.com>
2019-11-06 12:56:09 -0800

    Finished darts, all tests pass

--- darts/darts.go
+++ darts/darts.go
@@ -1,1 +1,21 @@
 package darts
+
+import (
+	"math"
+)
+
+func inCircle(radius int, x, y float64) bool {
+	distance := math.Sqrt((x * x) + (y * y))
+	return distance <= float64(radius)
+}
+
+func Score(x, y float64) int {
+	if inCircle(1, x, y) {
+		return 10
+	} else if inCircle(5, x, y) {
+		return 5
+	} else if inCircle(10, x, y) {
+		return 1
+	}
+	return 0
+}
