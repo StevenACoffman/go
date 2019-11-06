8a78f9ca2e5d0c32400686b7274d1795f8676343
Aasmund Eldhuset <aasmund.eldhuset@gmail.com>
2019-11-06 12:45:46 -0800

    Darts

--- darts/darts.go
+++ darts/darts.go
@@ -1,1 +1,16 @@
 package darts
+
+import "math"
+
+func Score(x, y float64) int {
+	r := math.Sqrt(x * x + y * y)
+	if r <= 1 {
+		return 10
+	} else if r <= 5 {
+		return 5
+	} else if r <= 10 {
+		return 1
+	} else {
+		return 0
+	}
+}
